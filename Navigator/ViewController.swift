e30154f73061c8c595b7737a1d9db3e42d1b6226
Roman-Fedotov-iOS <romafed23@gmail.com>
2022-03-14 16:00:41 +0200

    First

--- Navigator/ViewController.swift
+++ Navigator/ViewController.swift
@@ -6,14 +6,109 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mapVIew: MKMapView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var directionLbl: UILabel!
+    
+    let locationManager = CLLocationManager()
+    
+    var currentCoordinate: CLLocationCoordinate2D!
+    
+    var steps = [MKRoute.Step]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManager.requestAlwaysAuthorization()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        locationManager.startUpdatingLocation()
+        
     }
-
-
+    
+    func getDirection(to destination: MKMapItem ) {
+        let sourcePlacemark = MKPlacemark(coordinate: currentCoordinate)
+        let sourceMapItem = MKMapItem(placemark: sourcePlacemark)
+        let directionRequest = MKDirections.Request()
+        
+        directionRequest.source = sourceMapItem
+        directionRequest.destination = destination
+        directionRequest.transportType = .automobile
+        
+        let directions = MKDirections(request: directionRequest)
+        directions.calculate { response, _ in
+            guard let response = response else { return }
+            guard let primaryRoute = response.routes.first else { return }
+            self.mapVIew.addOverlay(primaryRoute.polyline)
+            
+            self.locationManager.monitoredRegions.forEach({ self.locationManager.stopMonitoring(for: $0) })
+            self.steps = primaryRoute.steps
+            
+            for i in 0 ..< primaryRoute.steps.count {
+                let step = primaryRoute.steps[i]
+                print(step.distance)
+                print(step.instructions)
+                let region = CLCircularRegion(center: step.polyline.coordinate , radius: 20, identifier: "\(i)")
+                self.locationManager.startMonitoring(for: region)
+                let circle = MKCircle(center: region.center, radius: region.radius)
+                self.mapVIew.addOverlay(circle)
+            }
+            
+            let initialMessage = "In \(self.steps[0].distance) meters, \(self.steps[0].instructions) then in \(self.steps[1].distance) meters, \(self.steps[1].instructions)"
+            self.directionLbl.text = initialMessage
+        }
+    }
+    
+    
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        manager.stopUpdatingLocation()
+        guard let currentLocation = locations.first else { return }
+        currentCoordinate = currentLocation.coordinate
+        mapVIew.userTrackingMode = .followWithHeading
+    }
+}
+
+extension ViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.endEditing(true)
+        let localSearchRequest = MKLocalSearch.Request()
+        localSearchRequest.naturalLanguageQuery = searchBar.text
+        let region = MKCoordinateRegion(center: currentCoordinate, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+        localSearchRequest.region = region
+        let localSearch = MKLocalSearch(request: localSearchRequest)
+        localSearch.start { response, _ in
+            guard let response = response else { return }
+//            print(response.mapItems)
+            guard let firstItem = response.mapItems.first else { return }
+            self.getDirection(to: firstItem)
+        }
+    }
+}
+
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay is MKPolyline {
+            let renderer = MKPolylineRenderer(overlay: overlay)
+            renderer.strokeColor = .blue
+            renderer.lineWidth = 5
+            return renderer
+        }
+        if overlay is MKCircle {
+            let renderer = MKCircleRenderer(overlay: overlay)
+            renderer.strokeColor = .red
+            renderer.fillColor = .red
+            renderer.alpha = 0.3
+            return renderer
+        }
+        
+        return MKOverlayRenderer()
+    }
+}
